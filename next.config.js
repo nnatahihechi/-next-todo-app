640162f72ad7ae427e6812dd6f77c3cee018c0d7
IHECHI <nnatahi57@gmail.com>
2022-07-10 23:25:15 +0100

    CONFIG

--- next.config.js
+++ next.config.js
@@ -1,1 +1,9 @@
-// export default {}
+// next.config.js
+export async function rewrites() {
+    return [
+        {
+            source: `pages/api/AddTodo`,
+            destination: "https://next-todo-app-vert.vercel.app/pages/api/AddTodo",
+        },
+    ];
+}
