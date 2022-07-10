1d198171de5e81bd11dd3ee70969605d1f682031
IHECHI <nnatahi57@gmail.com>
2022-07-10 23:30:15 +0100

    cors error bug fix

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ export async function rewrites() {
     return [
         {
             source: `pages/api/AddTodo`,
-            destination: "https://next-todo-app-vert.vercel.app/pages/api/AddTodo",
+            destination: "https://next-todo-app-vert.vercel.app/pages/api/addTodo",
         },
     ];
 }
