4b11e3daf39d332defb9c6bc0bfdaa01d2b4015d
IHECHI <nnatahi57@gmail.com>
2022-07-11 01:24:11 +0100

    Update

--- next.config.js
+++ next.config.js
@@ -3,13 +3,10 @@ module.exports = {
     async rewrites() {
         return [
           {
-            // source: `/api/AddTodo`,
             source: `https://next-todo-app-nine.vercel.app`,
-            // destination: "https://next-todo-app-nnatahihechi.vercel.app/api/TodoItem",
             destination: `/api/TodoItem`,
           },
         ]
       },
   };
 //   https://next-todo-6diztz6q3-nnatahihechi.vercel.app
-
